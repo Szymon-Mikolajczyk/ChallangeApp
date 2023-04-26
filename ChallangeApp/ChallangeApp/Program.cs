c9b7983ce530f898c1c467406b3aae278f3a1a4e
Szymon-Mikolajczyk <szymonm995@gmail.com>
2023-04-26 13:41:37 +0200

    Zadanie Dzień 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var age = 33;
+char sex = 'F'; // F - kobieta, M - mężczyzna
+
+if (sex == 'F')
+{
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33.");
+    }
+    else if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat.");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta lat 30 lub więcej.");
+    }
+}
+else if (age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna.");
+}
+else
+{
+    Console.WriteLine("Pełnoletni mężczyzna.");
+}
